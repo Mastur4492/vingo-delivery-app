5bacaa93e0d8992f44db77bc6144dc7e9b0d5711
Mastur <bikanerwalamastur13@gmail.com>
2026-03-12 22:43:24 +0530

    updated api

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4FxLx51CV45Rp9kGsVBYl8lO8-4tXEgU",
-  authDomain: "vingo-delivery-87205.firebaseapp.com",
-  projectId: "vingo-delivery-87205",
-  storageBucket: "vingo-delivery-87205.firebasestorage.app",
-  messagingSenderId: "77002873498",
-  appId: "1:77002873498:web:344f97c51130dfef041d23",
-  measurementId: "G-QW74DK2MN1"
+  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
